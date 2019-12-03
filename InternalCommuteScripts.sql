07190c17a48498622ba1c440b64c872bcbbacf76
abhiramtilak <46491513+abhiramtilak@users.noreply.github.com>
2019-12-03 19:00:25 +0530

    created ridestable

--- InternalCommuteScripts.sql
+++ InternalCommuteScripts.sql
@@ -13,3 +13,13 @@ CREATE TABLE `internalcommute`.`users` (
   `vehicle` VARCHAR(45) NULL,
   `availableSeats` VARCHAR(45) NULL,
   PRIMARY KEY (`userId`, `email`, `mobileNumber`));
+               
+  CREATE TABLE `internalcommute`.`rides` (
+  `rideId` INT NOT NULL,
+  `userId` VARCHAR(45) NOT NULL,
+  `vehicle` VARCHAR(45) NOT NULL,
+  `availableSeats` VARCHAR(45) NOT NULL,
+  `vehicleNumber` VARCHAR(45) NOT NULL,
+  `rideDate` DATETIME NOT NULL,
+  `rideTime` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`rideId`));
